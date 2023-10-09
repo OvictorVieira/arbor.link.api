28343f6d5008d6de3391c1249759b2368d69bdb4
Victor Vieira <victor.h.souza.vieira@gmail.com>
2023-10-09 19:11:50 -0300

    Refactors errors response

--- app/services/v1/partners/entity_parent_validator.rb
+++ app/services/v1/partners/entity_parent_validator.rb
@@ -4,10 +4,8 @@ class V1::Partners::EntityParentValidator
 
   def self.validate!(entity)
     if entity.class?
-      if entity.parent.nil?
+      if !Entity.exists?(entity.parent_id)
         raise CreateFailureError.new("A Class entity must have a parent (Network or School)")
-      elsif !Entity.exists?(entity.parent_id)
-        raise CreateFailureError.new("The specified parent does not exist")
       end
     end
   end
