d2aab6b8c79943fb555d406846524f08ab11292d
Victor Vieira <victor.h.souza.vieira@gmail.com>
2023-10-09 15:29:32 -0300

    Creates tests to entity_parent_validator

---
+++ spec/spec/services/v1/entity_parent_validator_spec.rb
@@ -0,0 +1,54 @@
+require 'rails_helper'
+
+RSpec.describe V1::EntityParentValidator do
+  describe '.validate!' do
+    let(:network) { create(:entity, entity_type: 'network') }
+    let(:school) { create(:entity, entity_type: 'school') }
+
+    context 'when entity is of type class' do
+      let(:class_entity) { build(:entity, entity_type: 'class') }
+
+      context 'and has no parent' do
+        it 'raises a CreateFailureError' do
+          expect {
+            described_class.validate!(class_entity)
+          }.to raise_error(CreateFailureError, "A Class entity must have a parent (Network or School)")
+        end
+      end
+
+      context 'and has a non-existent parent' do
+        before do
+          class_entity.parent_id = 9999 # some non-existent ID
+        end
+
+        it 'raises a CreateFailureError' do
+          expect {
+            described_class.validate!(class_entity)
+          }.to raise_error(CreateFailureError, "A Class entity must have a parent (Network or School)")
+        end
+      end
+
+      context 'and has a valid parent' do
+        before do
+          class_entity.parent = network
+        end
+
+        it 'does not raise an error' do
+          expect {
+            described_class.validate!(class_entity)
+          }.not_to raise_error
+        end
+      end
+    end
+
+    context 'when entity is not of type class' do
+      let(:school_entity) { build(:entity, entity_type: 'school') }
+
+      it 'does not raise an error' do
+        expect {
+          described_class.validate!(school_entity)
+        }.not_to raise_error
+      end
+    end
+  end
+end
