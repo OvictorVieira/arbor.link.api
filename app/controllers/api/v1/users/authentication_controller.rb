9afe78ff65fa5d0c638ba418b12c672010f36039
Victor Vieira <victor.h.souza.vieira@gmail.com>
2023-10-10 12:35:37 -0300

    Fix the jwt_secret path

--- app/controllers/api/v1/users/authentication_controller.rb
+++ app/controllers/api/v1/users/authentication_controller.rb
@@ -4,7 +4,7 @@ class Api::V1::Users::AuthenticationController < ApplicationController
   def login
     user = User.find_by(email: user_params[:email])
     if user&.authenticate(user_params[:password])
-      token = JWT.encode({ user_id: user.id }, Rails.application.secrets.jwt_secret, 'HS256')
+      token = JWT.encode({ user_id: user.id }, Rails.application.config.app.jwt.jwt_secret, 'HS256')
       render json: { token: token }, status: :ok
     else
       render json: { error: 'Unauthorized' }, status: :unauthorized
